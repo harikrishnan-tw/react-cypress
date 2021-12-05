73888ae72c415d7a8b5807b285995034d1825c95
harikrishnan-tw <86928653+harikrishnan-tw@users.noreply.github.com>
2021-12-05 22:36:10 +0530

    css in js for tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+type ButtonProps = {
+  color: "blue" | "red" | "purple";
+};
+
+const classes = {
+  button({ color }: ButtonProps): string {
+    return `bg-${color}-500 hover:bg-${color}-300 text-white font-bold rounded p-2`;
+  },
+};
+
 function App() {
-  return <>Added tailwind css</>;
+  return (
+    <button className={classes.button({ color: "purple" })}>Click me</button>
+  );
 }
 
 export default App;
